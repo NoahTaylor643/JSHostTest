70e2aac17fa858e1f7f8cf4206370f2d1ca195ed
NoahTaylor643 <noahtaylor@gdcit.com>
2025-04-29 14:41:28 -0400

    Update activelabel.js

--- activelabel.js
+++ activelabel.js
@@ -7,26 +7,4 @@ window.onload = function () {
       element.classList.add('active');
     });
   });  
-
-  
-  document.querySelectorAll('.form-input-wrapper-div').forEach(function(element) {
-    element.addEventListener('click', function(event) {
-      console.log('Div Test');
-      element.classList.add('active');
-    });
-  }); 
-
-  document.querySelectorAll('.form-input-element').forEach(function(element) {
-    element.addEventListener('click', function(event) {
-      console.log('input Test');
-      element.classList.add('active');
-    });
-  }); 
-  
-  document.querySelectorAll('.form-input-wrapper-element').forEach(function(element) {
-    element.addEventListener('click', function(event) {
-      console.log('label Test');
-      element.classList.add('active');
-    });
-  }); 
 };
