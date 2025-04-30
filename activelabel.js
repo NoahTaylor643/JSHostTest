d158d0a32749efcfd0f47c55e57dd1f38510e766
NoahTaylor643 <noahtaylor@gdcit.com>
2025-04-30 16:48:43 -0400

    Update activelabel.js

--- activelabel.js
+++ activelabel.js
@@ -1,23 +1,34 @@
-setTimeout(() => {
-  document.querySelectorAll('.form-input-element').forEach(function(el) {
-    const label = el.closest('label');
-    const span = label.querySelector('span');
+document.body.addEventListener("change", function(e) {
+  if (e.target.matches(".form-input-element")) {
+    const label = e.target.closest("label");
+    const span = label?.querySelector("span");
 
-    if (el.value) {
-      span.classList.add('active');
-    }
-
-    el.addEventListener('focus', () => {
-      span.classList.add('active');
-    });
-
-    el.addEventListener('blur', () => {
-      if (!el.value) {
-        span.classList.remove('active');
+    if (span) {
+      if (e.target.value) {
+        span.classList.add("active");
+      } else {
+        span.classList.remove("active");
       }
-    });
-  });
-}, 0);
+    }
+  }
+});
+
+document.body.addEventListener("focusin", function(e) {
+  if (e.target.matches(".form-input-element")) {
+    const label = e.target.closest("label");
+    const span = label?.querySelector("span");
+    span?.classList.add("active");
+  }
+});
+
+document.body.addEventListener("focusout", function(e) {
+  if (e.target.matches(".form-input-element") && !e.target.value) {
+    const label = e.target.closest("label");
+    const span = label?.querySelector("span");
+    span?.classList.remove("active");
+  }
+});
+
 
 
 
